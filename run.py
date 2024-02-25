fe87903ff0991efe180a55cdad716255d7da943c
Jack <jackmanges@gmail.com>
2024-02-24 22:40:56 -0500

    Added user account capability

--- run.py
+++ run.py
@@ -3,10 +3,27 @@
 
 import os
 
-from flask import Flask, g, json, render_template, request
+from flask import Flask, g, json, render_template, request, session, redirect
+from passlib.hash import pbkdf2_sha256
+from db import Database
+
+DATABASE_PATH = 'spellcaster.db'
 
 app = Flask(__name__)
+app.secret_key = b'demokeynotreal!'
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = Database(DATABASE_PATH)
+    return db
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
@@ -22,18 +39,44 @@ def about():
 def spelllist():
     return render_template('bikes.html')
 
-# From old template
-#@app.route('/about2')
-#def about2():
-#    data = {
-#        'related': [
-#            {'name': 'Rent A Goat', 'url': 'http://rentagoat.com'},
-#            {'name': 'We Rent Goats', 'url': 'http://werentgoats.com'},
-#            {'name': 'Goat Yoga', 'url': 'https://goatyoga.net/'}
-#        ]
-#    }
-#    return render_template('about2.html', data=data)
+@app.route('/create_user', methods=['GET', 'POST'])
+def create_user():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        username = request.form.get('username')
+        typed_password = request.form.get('password')
+        if name and username and typed_password:
+            encrypted_password = pbkdf2_sha256.hash(typed_password)
+            get_db().create_user(name, username, encrypted_password)
+            return redirect('/login')
+    return render_template('create_user.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = None
+    if request.method == 'POST':
+        username = request.form.get('username')
+        typed_password = request.form.get('password')
+        if username and typed_password:
+            user = get_db().get_user(username)
+            if user:
+                if pbkdf2_sha256.verify(typed_password, user['encrypted_password']):
+                    session['user'] = user
+                    return redirect('/')
+                else:
+                    message = "Incorrect password, please try again"
+            else:
+                message = "Unknown user, please try again"
+        elif username and not typed_password:
+            message = "Missing password, please try again"
+        elif not username and typed_password:
+            message = "Missing username, please try again"
+    return render_template('login.html', message=message)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
